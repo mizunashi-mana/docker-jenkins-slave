1bd0b833c749ff5736a7164c330ab62554cbe340
Mizunashi Mana <mizunashi_mana@mma.club.uec.ac.jp>
2015-07-15 23:46:37 +0900

    add Dockerfile
    And, add installion steps and id example

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM java:latest
+MAINTAINER Mizunashi Mana <mizunashi_mana@mma.club.uec.ac.jp>
+
+RUN apt-get update \
+ && apt-get install -y openssh-server sudo \
+ && rm -rf /var/lib/apt/lists/*
+
+ENV JENKINS_WORKUSER="jenkins" \
+    JENKINS_WORKSPACE="/var/jenkins_ws" \
+    SETUP_DIR="/var/chche/jenkins"
+
+COPY setup/ ${SETUP_DIR}/
+RUN bash ${SETUP_DIR}/install.sh
+
+EXPOSE 22/tcp
+
+VOLUME ["${JENKINS_WORKSPACE}"]
+WORKDIR ${JENKINS_WORKSPACE}
+
---
+++ setup/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+rm -rf /etc/ssh/ssh_host_*_key /etc/ssh/ssh_host_*_key.pub
+
+useradd -d "${JENKINS_WORKSPACE}" -u 1000 -m -s /bin/bash ${JENKINS_WORKUSER}
+echo "${JENKINS_WORKUSER}:jenkins" | chpasswd
+
+rm -rf ${JENKINS_WORKSPACE}/.ssh
+sudo -HEu ${JENKINS_WORKUSER} mkdir -p ${JENKINS_WORKSPACE}/.ssh
+
+sed 's/UsePAM yes/UsePAM no/' -i /etc/ssh/sshd_config
+sed 's/UsePrivilegeSeparation yes/UsePrivilegeSeparation no/' -i /etc/ssh/sshd_config
+echo "UseDNS no" >> /etc/ssh/sshd_config
+
+sed 's/#PasswordAuthentication yes/PasswordAuthentication no/' -i /etc/ssh/sshd_config
+
+sed 's/LogLevel INFO/LogLevel VERBOSE/' -i /etc/ssh/sshd_config
+
+mkdir -p /var/run/sshd
